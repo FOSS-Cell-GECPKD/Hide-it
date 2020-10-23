6d59c2f0e9abe9f9700254721b313ae69476d266
VipinVIP <kpvipin33@gmail.com>
2020-10-23 22:33:44 +0530

    Added an info and github link

--- src/components/OperativeLayout.js
+++ src/components/OperativeLayout.js
@@ -17,8 +17,8 @@ export default function Operativelayout() {
   return (
     <div style={{ width: '98%', maxWidth: '500px', margin: 'auto' ,height:"62vh"}}>
       <Form className="m-1 p-1">
-        <InputBox value={secret} placeholder="Enter your secret text here " onChange={(e) => setSecret(e.target.value)} />
-        <InputBox value={mask} placeholder="Enter your Mask Text here " onChange={(e) => setMask(e.target.value)} />
+        <InputBox value={secret} placeholder="Enter message to Encode/Decode" onChange={(e) => setSecret(e.target.value)} />
+        <InputBox value={mask} placeholder="Enter your Mask Text here(optional)" onChange={(e) => setMask(e.target.value)} />
 
         <div className="d-flex justify-content-center">
           <Button onClick={() => { setEncodedShow(true); setModalOpen(true) }} className="m-2">Encode</Button>
